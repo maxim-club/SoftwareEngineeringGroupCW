c774e84c104c7320fbd4432a3c2bf00cb2662919
Marcel Lanecki <mdlanecki@icloud.com>
2025-12-03 12:55:48 +0000

    Initial commit of IntelliJ Gradle Groovy project

---
+++ src/test/java/com/studyspaces/spacefinder/SpacefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyspaces.spacefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacefinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
