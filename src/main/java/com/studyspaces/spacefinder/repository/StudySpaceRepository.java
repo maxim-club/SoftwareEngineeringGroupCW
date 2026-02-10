4003474c03cc1980d6ecc272009a2860d5bbdb3b
Marcel Lanecki <mdlanecki@icloud.com>
2026-02-10 13:10:14 +0000

    Fixed Mr Budzak's silly NeoVim Errors

--- src/main/java/com/studyspaces/spacefinder/repository/StudySpaceRepository.java
+++ src/main/java/com/studyspaces/spacefinder/repository/StudySpaceRepository.java
@@ -1,6 +1,5 @@
-package com.studyspaces.spacefinder;
+package com.studyspaces.spacefinder.repository;
 
-import com.studyspaces.spacefinder.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
