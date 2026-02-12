9298ad309d2785ea0c160b139a6561fb8a606e18
Marcel Lanecki <mdlanecki@icloud.com>
2026-02-12 14:34:49 +0000

    Renamed Occupancy models

--- src/main/java/com/studyspaces/spacefinder/repository/HistoricOccupancyRepository.java
+++ src/main/java/com/studyspaces/spacefinder/repository/HistoricOccupancyRepository.java
@@ -3,8 +3,8 @@ package com.studyspaces.spacefinder.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.studyspaces.spacefinder.model.OccupancyRecord;
+import com.studyspaces.spacefinder.model.RoomOccupancyRecord;
 
 @Repository
-public interface HistoricOccupancyRepository extends MongoRepository<OccupancyRecord, String> {
+public interface HistoricOccupancyRepository extends MongoRepository<RoomOccupancyRecord, String> {
 }
