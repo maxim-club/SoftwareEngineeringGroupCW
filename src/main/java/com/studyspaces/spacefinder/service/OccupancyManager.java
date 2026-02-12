b95d7cecd7a83035d7b5e022ec48206f11acb5af
Marcel Lanecki <mdlanecki@icloud.com>
2026-02-12 15:56:19 +0000

    Added new OccupancyManager method and test scripts

--- src/main/java/com/studyspaces/spacefinder/service/OccupancyManager.java
+++ src/main/java/com/studyspaces/spacefinder/service/OccupancyManager.java
@@ -5,6 +5,8 @@ import com.studyspaces.spacefinder.model.*;
 
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 
 @Service
 public class OccupancyManager {
@@ -15,13 +17,26 @@ public class OccupancyManager {
         this.repo = repo;
     }
 
-    public Occupancy getLastOccupancy(String id) {
-        OccupancyRecord lastOccupancy = repo.findLastRoomOccupancy(id).isPresent() ? repo.findLastRoomOccupancy(id).get() : null;
+    // Retrieves latest occupancy level for a specified room
+    public Occupancy getLastOccupancy(String Roomid) {
+        return repo.findLastRoomOccupancy(Roomid)
+                .map(OccupancyRecord::getOccupancyLevel)
+                .orElse(null);
+    }
+
+    public long whenLastOccupancyWasAdded(String Roomid ) {
+        OccupancyRecord lastOccupancy =
+                repo.findLastRoomOccupancy(Roomid).isPresent()
+                        ? repo.findLastRoomOccupancy(Roomid).get()
+                        : null;
 
         if (lastOccupancy == null){
-            return null;
+            return -1;
         }
 
-        return lastOccupancy.getOccupancyLevel();
+        long lastTimestamp = lastOccupancy.getTimestamp();
+        long now = Instant.now().getEpochSecond();
+
+        return now - lastTimestamp;
     }
 }
