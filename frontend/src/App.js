8f6125d172a4c4bf554dea3af94e5fecb980e613
noor-alabdullatif <naa88@bath.ac.uk>
2026-02-15 13:59:15 +0000

    Add SearchBar component and integrate into Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppNavbar from './components/Navbar';
 import Home from './pages/Home';
 import Search from './pages/Search';
-import BackendTest from "./components/BackendTest";
-
 
 function App() {
     return (
@@ -12,7 +10,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/search" element={<Search />} />
-                <Route path="/backendtest" element={<BackendTest />} />
             </Routes>
         </Router>
     );
