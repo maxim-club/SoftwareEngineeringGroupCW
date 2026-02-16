fe950a784af2e5be88ad7ca1f25f6a11a419a586
KhemmakornJ <engeng.khammakorn@gmail.com>
2026-02-16 17:21:23 +0000

    Map view

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "leaflet/dist/leaflet.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
