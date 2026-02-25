6716427b531735cb348b27c6c8474be28420025c
KhemmakornJ <engeng.khammakorn@gmail.com>
2026-02-25 10:31:07 +0000

    update nav and map

--- frontend/src/pages/Map-trial.jsx
+++ frontend/src/pages/Map-trial.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
 import StudySpacesMap from "../components/StudySpacesMap";
 
-function MapView() {
+export default function MapPage() {
+  const [q, setQ] = useState("");
+
   return (
-    <div style={{ padding: 16 }}>
-      <h2>Study Spaces Map</h2>
-      <StudySpacesMap />
+    <div className="map-page">
+      {/* floating search */}
+      <div className="map-search">
+        <SearchBar
+          value={q}
+          onChange={setQ}
+          onFilterClick={() => console.log("filter")}
+          placeholder="Search..."
+          onSubmit={() => console.log("submit", q)}
+        />
+      </div>
+
+      {/* map goes behind */}
+      <div className="map-full">
+        <StudySpacesMap />
+      </div>
     </div>
   );
 }
-
-export default MapView;
