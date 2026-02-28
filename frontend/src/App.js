aaac1364ab1805bc0aba03c75fa0e9b8723197f3
marta <Isaree2348@gmail.com>
2026-02-28 23:42:43 +0000

    Add intro page with illustration and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Search from "./pages/Search";
 import BackendTest from "./components/BackendTest";
 import LineTrial from "./pages/Line-trial";
 import MapView from "./pages/Map-trial";
+import Intro from './pages/Intro';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 import Filters from "./pages/Filters";
 import FilteredResults from "./pages/FilteredResults";
@@ -18,6 +21,12 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Routes WITHOUT navbar */}
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/space/:id" element={<SpaceDetailPage />} />
+
         {/* Routes WITH navbar */}
         <Route element={<LayoutWithNavbar />}>
           <Route path="/" element={<Home />} />
@@ -25,17 +34,11 @@ function App() {
           <Route path="/backendtest" element={<BackendTest />} />
           <Route path="/line-chart" element={<LineTrial />} />
           <Route path="/mapview" element={<MapView />} />
-
           <Route path="/filters" element={<Filters />} />
           <Route path="/filtered-results" element={<FilteredResults />} />
-
           <Route path="/profile" element={<UserProfile />} />
-          {/*<Route path="/saved" element={<SavedStudySpot />} />*/}
           <Route path="/checkedin" element={<CheckedInScreen />} />
         </Route>
-
-        {/* Route WITHOUT navbar */}
-        <Route path="/space/:id" element={<SpaceDetailPage />} />
       </Routes>
     </Router>
   );
