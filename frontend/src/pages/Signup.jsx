828a947693ae8f6e3562127d6955cf220c57553e
marta <Isaree2348@gmail.com>
2026-02-28 23:42:43 +0000

    Add intro page with illustration and navigation

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Signup() {
+  return (
+    <Container style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Signup Page</h1>
+      <p>Signup form coming soon...</p>
+    </Container>
+  );
+}
+
+export default Signup;
