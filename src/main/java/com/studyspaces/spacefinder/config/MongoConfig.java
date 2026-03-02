c6764293786f89c2442559b98ecd5ef18ecb081d
AmyPanprasong <pakornpan4126@gmail.com>
2026-03-02 11:29:59 +0000

    change ScheduleEntry to match database

---
+++ src/main/java/com/studyspaces/spacefinder/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package com.studyspaces.spacefinder.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Override
+    protected String getDatabaseName() {
+        return "Data";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+}
