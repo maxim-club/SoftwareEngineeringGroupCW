d4b52022b1ae0e54cc8f0a0a9f4985ee1d0c4ce7
Marcel Lanecki <mdlanecki@icloud.com>
2026-03-03 14:12:06 +0000

    updating branch to track main

--- src/main/java/com/studyspaces/spacefinder/SpacefinderApplication.java
+++ src/main/java/com/studyspaces/spacefinder/SpacefinderApplication.java
@@ -21,48 +21,4 @@ public class SpacefinderApplication {
 
         SpringApplication.run(SpacefinderApplication.class, args);
     }
-
-    // Integration test for mongoDB with the repositories
-    @Bean
-    CommandLineRunner testRepositories(
-            StudySpaceRepository studySpaceRepository,
-            RealTimeOccupancyRepository occupancyRepository,
-            UserRepository userRepository
-    ) {
-        return args -> {
-
-            System.out.println("=== Testing Mongo Repositories ===");
-            studySpaceRepository.findAll()
-                    .forEach(s -> System.out.println(s.getId()));
-
-            // Test StudySpace insert
-            StudySpaceProfile space = new StudySpaceProfile();
-            space.setId("test-room");
-            space.setRoomLocation("Test Building - Room 101");
-
-            studySpaceRepository.save(space);
-
-            // Test fetch
-            var found = studySpaceRepository.findById("test-room");
-            System.out.println("Found space: " + found);
-
-            // Test User insert
-            UserRecord user = new UserRecord();
-            user.setId("test-user");
-            user.setUsername("demo");
-            user.setPassword("hashed-password");
-
-            userRepository.save(user);
-
-            System.out.println("Users count: " + userRepository.count());
-
-            // Test occupancy lookup
-            boolean exists =
-                    occupancyRepository.existsById("test-room");
-
-            System.out.println("Occupancy exists: " + exists);
-
-            System.out.println("=== Repository test complete ===");
-        };
-    }
 }
